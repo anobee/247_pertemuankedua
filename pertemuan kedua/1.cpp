2dd8ddad21fcfcb1809e4f9e0c2b9369233b3935
dicky <161241858+anobee@users.noreply.github.com>
2024-03-21 13:18:30 +0700

    Insertion.cpp
    Not bad

--- pertemuan kedua/1.cpp
+++ pertemuan kedua/1.cpp
@@ -1,25 +1,61 @@
-// pertemuan kedua.cpp : Defines the entry point for the application.
-//
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    int bilangan;
+int i, j, n, temp;
+int arr[20];
 
-    srand(time(0));
 
-    bilangan = rand() % 10;
-
-    cout << "niai awal : " << bilangan << endl;
-
-    if (bilangan >= 5)
-
-    {
-        bilangan = 2 * bilangan;
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
     }
-
-    cout << "nilai sekarang = " << bilangan << endl;
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
 
+void InsertionSort() {
+        for (i = 1; i <= n - 1; i++) { //step 1
+            temp = arr[i]; //step 2
+
+            j = i - i; // step 3
+
+            while (j >= 0 && arr[j] > temp) //step 4 
+            {
+                arr[j + 1] = arr[j]; //step 4a
+                j--; //step 4b
+            }
+            arr[j + 1] = temp;//step 5
+        }
+
+    }void display() {
+        cout << endl;
+        cout << "=================================" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "=================================" << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j] << endl; //output setiap element array pada garis baru
+        }
+        cout << "jumlah pass = " << n - 1 << endl;
+        cout << endl;
+    }
+
+    int main() {
+        input();
+        InsertionSort();
+        display();
+
+        system("pause");
+        return 0;
+}
